6db86c5fd391daef76928b2544801110ad5f750d
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2022-12-23 13:33:13 +0100

    Brain Sciences : 2022-12-23_video-abstract.py

--- 2022-12-23_video-abstract.py
+++ 2022-12-23_video-abstract.py
@@ -87,93 +87,6 @@ for i_sub, subtitle in enumerate(intro_subs):
     clip.append(sub)
 
 
-
-# fig_name = {"BSM":['BSM.mp4'], # 30''
-#             "Bet_eyeMvt":['eyeMvt.mp4',  # 10''
-#                       # '1_B_Trace_moyenne.png',
-#                       # 'demo_fit.png'
-#                          'Bet.mp4'],  # 15''
-#             "BBCP":['3_BCP_model.png', 
-#                     '3_BCP_readouts.png',
-#                     '2_results_enregistrement.mp4', # 30''
-#                     '4_A_result_psycho_aSPEM.png',
-#                     '4_B_result_psycho_bet.png',
-#                     '5B_inter-individual_differences_fit.png',
-#                     '5A_inter-individual_differences_fit.png']}
-
-# duration_dict = {'3_BCP_model.png':12, 
-#                  '3_BCP_readouts.png':9,
-#                  '4_A_result_psycho_aSPEM.png':7,
-#                  '4_B_result_psycho_bet.png':7,
-#                  '5A_inter-individual_differences_fit.png':6,
-#                  '5B_inter-individual_differences_fit.png':6}
-
-# # LES CHOSES SERIEUSES !
-# texts = ["BSM", "Bet_eyeMvt", "BBCP"]
-# colors = ['black', 'orange', 'blue', 'red']
-# subtitles = {}
-# subtitles['BSM.mp4'] = ["Take for instance the case of a doctor who tests...", # 
-#                "...her patients to check if they have a flu or not.", 
-#                "The underlying cause for these observations (Top graph)...", 
-#                # "...might be caused by a switch in probability rate (Bottom)...",
-#                "...might be a sudden increase in the occurrence of flu...",
-#                "...from a normal situation (low p) to an outbreak (high p).", 
-#                "In the longer-term, we may understand volatility as...", 
-#                "...different epochs, each with a stationary rate of cases and...", 
-#                "...separated by switches which occur at random times.", 
-#                # "...but with a given hazard rate (= rate of switches)."
-#                        ]
-
-# subtitles['eyeMvt.mp4'] = ["How do humans behave to anticipate future outcomes?", 
-#                            "We tested this by showing trial sequences...", 
-#                            "...drawn from such a switching process.", 
-#                            "Each trial consists of a dot on the screen...", 
-#                            "... which moves either left or right.", 
-#                            "First, we recorded the eye movements of observers....", 
-#                            "...as it is known that anticipatory movements occur...", 
-#                            "...in the direction which is the most likely."]
-
-# subtitles['Bet.mp4'] = ["On another day, we tested the same observer...", 
-#                "...which had to explicitly adjust a cursor...", 
-#                "...to guess for the next outcome ..", 
-#                "... (from 'left', 'unsure', to 'right')."]
-
-# subtitles['3_BCP_model.png'] = ["To understand the strategy which is used by the observers,...", 
-#                                 "...we created the model of an agent which represents...", 
-#                                 "...different alternative beliefs of the time since a switch.",
-#                                 "We proved mathematically that this model is the best possible...", 
-#                                 "...you can get when knowing the switching nature of process."
-#                     ]
-# subtitles['3_BCP_readouts.png'] = ["We compared the results of this model (in green)...",
-#                                    "...with a model which simply averages the last trials...", 
-#                                    "...to estimate the current bias (leaky integrator, in orange).", 
-
-#                                    ]
-# subtitles['2_results_enregistrement.mp4'] = [
-#                "Results show that observers were efficient in inferring...", 
-#                "...the next outcome both explicitly with the cursor (bottom)...", 
-#                "...and also with the eye movements (middle). We...", 
-#                "...showed (top) that the model inferring switches (orange)...", 
-#                "...performed better than the leaky integrator (green),...", 
-#                "...in particular after switches.", 
-#                ]
-
-# subtitles['4_A_result_psycho_aSPEM.png'] = ["This better fit demonstrates ...", 
-#                "...that humans adapt to the volatility of the environment and ..."]
-# subtitles['4_B_result_psycho_bet.png'] = ["...have some representation for this volatility,",
-#                                          "an adaptive strategy richer than previously assumed."]
-# subtitles['5B_inter-individual_differences_fit.png'] = [
-#                 "Moreover, adjusting the free parameter of the model...", 
-#                 "...from more volatile (bottom row)...", 
-#                 "...to more conservative (top row),...", 
-#                "...we could find the best fit hazard rate for each individual."]
-# subtitles['5A_inter-individual_differences_fit.png'] = [
-#                "This shows that different individuals have different...",
-#                 "...compromise between exploration and the exploitation...", 
-#                "...a hallmark of their inter-individual differences."]
-
-
-
 chapters = {'VS': dict(title="Visual system", color='black'), 
             'poly': dict(title="Polychrony", color='orange'), 
             'neuro': dict(title="Neurobiology", color='red'), 
@@ -202,7 +115,8 @@ chapters['DVS']['content'] = [dict(figure='figures/visual-latency.jpg', duration
 txt_opts = dict(fontsize=65, bg_color='white', align='center', **opt_st)
 sub_opts = dict(fontsize=28, align='South', color='white', **opt_st)
 
-for chapter in chapters:
+for chapter_key in chapters.keys():
+    chapter = chapters[chapter_key]
     duration = 1
     txt = TextClip(chapter[title], color=chapter[color], **txt_opts).set_start(t).set_duration(duration)
     t += duration
